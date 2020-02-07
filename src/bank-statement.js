092835beaab2e9ff28f2854dd7bc89c675cae898
Andrei_Gordeev <Andrei_Gordeev@epam.com>
2020-02-07 19:23:05 +0300

    stored values from js to table and edited css a little

--- src/bank-statement.js
+++ src/bank-statement.js
@@ -1,5 +1,34 @@
 var x = document.querySelector('tbody'); // gets elements with css selector
-const cell = document.createElement('td');
-cell.innerText = 'hello'
-x.appendChild(cell);
-console.log(x);
+for(var i = 0; i < myStatementData.length; i++){
+    const dateString = myStatementData[i].date;
+    const indexT = dateString.indexOf('T');
+    const amount = myStatementData[i].amount;
+
+    const dateCell = document.createElement('td');
+    dateCell.innerText = (dateString).substring(0, indexT);
+    x.appendChild(dateCell);
+
+    const timeCell = document.createElement('td');
+    timeCell.innerText = (dateString).substring(indexT + 1);
+    x.appendChild(timeCell);
+
+    const typeCell = document.createElement('td');
+    typeCell.innerText = myStatementData[i].type;
+    x.appendChild(typeCell);
+
+    if (amount > 0) {
+        const incomeCell = document.createElement('td');
+        incomeCell.innerText = amount;
+        x.appendChild(incomeCell);
+        x.appendChild(document.createElement('td'));
+    }
+    else {
+        x.appendChild(document.createElement('td'));
+        const outcomeCell = document.createElement('td');
+        outcomeCell.innerText = Math.abs(amount);
+        x.appendChild(outcomeCell);
+    }
+
+    const cell = document.createElement('tr');
+    x.appendChild(cell);
+}
