61c40887a19ef36679327983486f8d2a22a4da5a
Andrei_Gordeev <Andrei_Gordeev@epam.com>
2020-02-10 13:17:09 +0300

    added some locale formatting

--- src/bank-statement.js
+++ src/bank-statement.js
@@ -1,34 +1,52 @@
 var x = document.querySelector('tbody'); // gets elements with css selector
 for(var i = 0; i < myStatementData.length; i++){
-    const dateString = myStatementData[i].date;
-    const indexT = dateString.indexOf('T');
-    const amount = myStatementData[i].amount;
+    const date = myStatementData[i].date;
+    var amount = myStatementData[i].amount;
+    var dateString = parseDate(date);
+    var timeString = parseTime(date);
 
     const dateCell = document.createElement('td');
-    dateCell.innerText = (dateString).substring(0, indexT);
+    dateCell.innerText = dateString;
     x.appendChild(dateCell);
 
     const timeCell = document.createElement('td');
-    timeCell.innerText = (dateString).substring(indexT + 1);
+    timeCell.innerText = timeString;
     x.appendChild(timeCell);
 
     const typeCell = document.createElement('td');
     typeCell.innerText = myStatementData[i].type;
     x.appendChild(typeCell);
 
+    var amountString;
     if (amount > 0) {
         const incomeCell = document.createElement('td');
-        incomeCell.innerText = amount;
+        amountString = amount.toLocaleString('ru-RU') + ' ₽';
+        incomeCell.innerText = amountString;
         x.appendChild(incomeCell);
         x.appendChild(document.createElement('td'));
     }
     else {
+        amount = Math.abs(amount);
+        amountString = amount.toLocaleString('ru-RU') + ' ₽';
         x.appendChild(document.createElement('td'));
         const outcomeCell = document.createElement('td');
-        outcomeCell.innerText = Math.abs(amount);
+        outcomeCell.innerText = amountString;
         x.appendChild(outcomeCell);
     }
 
     const cell = document.createElement('tr');
     x.appendChild(cell);
+}
+
+function parseDate(dateStr){
+    var date = new Date(dateStr);
+    var options = {year: 'numeric', month: 'numeric', day: 'numeric'};
+    return date.toLocaleString('ru-RU', options);
+
+}
+
+function parseTime(dateStr){
+    var date = new Date(dateStr);
+    var options = {hour: 'numeric', minute: 'numeric', second: 'numeric'};
+    return date.toLocaleTimeString('ru-RU', options);
 }
