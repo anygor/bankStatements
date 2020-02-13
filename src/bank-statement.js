8f2c6453a35eed7f2964f922485df1da6ed524bb
Andrei_Gordeev <Andrei_Gordeev@epam.com>
2020-02-13 12:22:15 +0300

    sorted array and no vars

--- src/bank-statement.js
+++ src/bank-statement.js
@@ -1,10 +1,5 @@
 const myStatementDataSorted = myStatementData.sort(compare);
 
-<<<<<<< HEAD
-=======
-console.log(myStatementDataSorted);
-
->>>>>>> 2fbb914e5f775c626a04422874d4868ac3f27916
 function compare(a, b){
     const dateA = a.date;
     const dateB = b.date;
@@ -150,11 +145,7 @@ function nogroup(){
 
     const x = document.querySelector('tbody');
 
-<<<<<<< HEAD
     for(let i = 0; i < myStatementDataSorted.length; i++){
-=======
-    for(var i = 0; i < myStatementDataSorted.length; i++){
->>>>>>> 2fbb914e5f775c626a04422874d4868ac3f27916
 
         const currentRow = document.createElement('tr');
         x.appendChild(currentRow);
@@ -201,17 +192,10 @@ function group(){
 
     let sumDay = new Map();
 
-<<<<<<< HEAD
     for (let i = 0; i < myStatementDataSorted.length; i++){
         let amount = myStatementDataSorted[i].amount;
         const date = myStatementDataSorted[i].date;
         const dateString = parseDate(date);
-=======
-    for (var i = 0; i < myStatementDataSorted.length; i++){
-        let amount = myStatementDataSorted[i].amount;
-        const date = myStatementDataSorted[i].date;
-        var dateString = parseDate(date);
->>>>>>> 2fbb914e5f775c626a04422874d4868ac3f27916
 
         if(!sumDay.has(dateString)){
             sumDay.set(dateString, amount);
